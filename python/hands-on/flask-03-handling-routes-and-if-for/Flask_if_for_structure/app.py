b23f89eea62b08e20b1debe3e5bae47009204cb0
sizin github kullanıcı adı <you@example.com>
2021-10-06 20:45:22 +0300

    flask-03-if-for added

---
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+   first = 'This is my first conditions experience'
+   return render_template('index.html', message = first)
+
+
+@app.route('/halil')
+def mylist():
+   names = ["Ted", "Salih", "Tarkan", "Sergio", "Ahmet Arif"]
+   return render_template('body.html', object = names)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
